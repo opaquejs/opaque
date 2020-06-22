67267cedec67cff71d20682931b1162b531ce793
Sebastian Dittrich <sebastian.dittrich@live.de>
2020-06-22 21:52:33 +0200

    Added types to exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { attribute, Model } from './Model'
-import { Query, MappedQuery } from './Query'
-import { IdentifiableStorageAdapter, ReactiveStorageAdapter, ThrottledReactiveStorageAdapter } from './Storage'
+import { Query, MappedQuery, Refreshable, Refreshes, Filter } from './Query'
+import { IdentifiableStorageAdapter, ReactiveStorageAdapter, ThrottledReactiveStorageAdapter, Attribute, Attributes, StorageAdapter, Identifiable } from './Storage'
 import { TestStorageAdapter } from './Tests'
 
 export {
+    Identifiable,
+    StorageAdapter,
+    Refreshable,
+    Refreshes, 
+    Filter,
+    Attribute,
+    Attributes,
     Model,
     attribute,
     Query,
