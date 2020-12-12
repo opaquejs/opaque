b09397a0c9db1ae3c0da380d668b7477ed3f6ae9
Sebastian Dittrich <sebastian.dittrich@live.de>
2020-12-12 22:26:15 +0100

    Changed adapter integration

--- src/Adapter.ts
+++ src/Adapter.ts
@@ -1,29 +1,29 @@
-import { OpaqueModel } from "./Model";
-import { IdType, ModelAttributes } from "./Contracts";
 import { RootQuery } from "./QueryBuilder";
+import { OpaqueModel } from "./Model";
+import { ModelAttributes } from "./Contracts";
 
-export interface OpaqueAdapter<Attributes extends object> {
-    create(model: Attributes): Promise<Attributes>
-    read(query: RootQuery<Attributes>): Promise<Attributes[]>
-    update(query: RootQuery<Attributes>, data: Partial<Attributes>): Promise<void>
-    delete(query: RootQuery<Attributes>): Promise<void>
+export interface OpaqueAdapter<Model extends typeof OpaqueModel> {
+    create(model: ModelAttributes<InstanceType<Model>>): Promise<ModelAttributes<InstanceType<Model>>>
+    read(query: RootQuery<ModelAttributes<InstanceType<Model>>>): Promise<ModelAttributes<InstanceType<Model>>[]>
+    update(query: RootQuery<ModelAttributes<InstanceType<Model>>>, data: Partial<ModelAttributes<InstanceType<Model>>>): Promise<void>
+    delete(query: RootQuery<ModelAttributes<InstanceType<Model>>>): Promise<void>
 }
 
-export interface OpaqueAdapterConstructor<Attributes extends object> {
-    new(model: Attributes): OpaqueAdapter<Attributes>
+export interface OpaqueAdapterConstructor<Model extends typeof OpaqueModel> {
+    new(model: Model): OpaqueAdapter<Model>
 }
 
-export class NoOpAdapter<Model extends object> implements OpaqueAdapter<Model> {
-    async create(data: Model) {
+export class NoOpAdapter<Model extends typeof OpaqueModel> implements OpaqueAdapter<Model> {
+    async create(data: ModelAttributes<InstanceType<Model>>) {
         return data
     }
-    async read(query: RootQuery<Model>) {
-        return [] as Model[]
+    async read(query: RootQuery<ModelAttributes<InstanceType<Model>>>) {
+        return [] as ModelAttributes<InstanceType<Model>>[]
     }
-    async delete(query: RootQuery<Model>) {
+    async delete(query: RootQuery<ModelAttributes<InstanceType<Model>>>) {
 
     }
-    async update(query: RootQuery<Model>) {
+    async update(query: RootQuery<ModelAttributes<InstanceType<Model>>>) {
 
     }
 }
