c481b4603472ef0a097fa63b7f63f848254d043f
Sebastian Dittrich <sebastian.dittrich@live.de>
2021-01-12 17:16:48 +0100

    Fixed serialization issue with "in" query

--- src/QueryBuilder.ts
+++ src/QueryBuilder.ts
@@ -1,70 +1,107 @@
-import { OpaqueModel } from "./Model";
-import { QueryBuilderContract, Query, ReverseComparisonTypes, Comparison } from "./contracts/QueryBuilderContracts";
-import { ModelAttributes } from "./contracts/ModelContracts";
-import { OpaqueRow, SyncReadAdapterContract, SyncWriteAdapterContract } from "./contracts/AdapterContracts";
+import { OpaqueModel } from './Model';
+import {
+  QueryBuilderContract,
+  Query,
+  ReverseComparisonTypes,
+  Comparison,
+} from './contracts/QueryBuilderContracts';
+import { ModelAttributes } from './contracts/ModelContracts';
+import {
+  OpaqueRow,
+  SyncReadAdapterContract,
+  SyncWriteAdapterContract,
+} from './contracts/AdapterContracts';
 
-export class QueryBuilder<Model extends (new () => OpaqueModel) & typeof OpaqueModel> implements QueryBuilderContract<InstanceType<Model>> {
+export class QueryBuilder<
+  Model extends (new () => OpaqueModel) & typeof OpaqueModel
+> implements QueryBuilderContract<InstanceType<Model>> {
+  constructor(
+    public model: Model,
+    public $query: Query<ModelAttributes<InstanceType<Model>>> = {}
+  ) {}
 
-    constructor(public model: Model, public $query: Query<ModelAttributes<InstanceType<Model>>> = {}) {
+  subQuery(query: Query<ModelAttributes<InstanceType<Model>>>) {
+    return new (this.constructor as any)(this.model, query) as this;
+  }
 
-    }
+  where<
+    Attributes extends ModelAttributes<InstanceType<Model>>,
+    Attribute extends keyof Attributes,
+    Key extends keyof ReverseComparisonTypes<Attributes[Attribute]>
+  >(
+    attribute: Attribute,
+    operator: Key,
+    value: ReverseComparisonTypes<Attributes[Attribute]>[Key]
+  ) {
+    const comparatorKey = Object.entries(Comparison).find(
+      ([_, literal]) => literal == operator
+    )![0];
+    const serialized =
+      operator == Comparison._in
+        ? (value as any[]).map((value) =>
+            this.model.$serializeAttribute(attribute as string, value)
+          )
+        : this.model.$serializeAttribute(attribute as string, value);
+    return this.subQuery({
+      ...this.$query,
+      [attribute]: { [comparatorKey]: serialized },
+    });
+  }
 
-    subQuery(query: Query<ModelAttributes<InstanceType<Model>>>) {
-        return new (this.constructor as any)(this.model, query) as this
-    }
+  limit(_limit: number) {
+    return this.subQuery({
+      ...this.$query,
+      _limit,
+    });
+  }
 
-    where<Attributes extends ModelAttributes<InstanceType<Model>>, Attribute extends keyof Attributes, Key extends keyof ReverseComparisonTypes<Attributes[Attribute]>>(attribute: Attribute, operator: Key, value: ReverseComparisonTypes<Attributes[Attribute]>[Key]) {
-        return this.subQuery({
-            ...this.$query,
-            [attribute]: {
-                [Object.entries(Comparison).find(([_, literal]) => literal == operator)![0]]: this.model.$serializeAttribute(attribute as string, value)
-            }
-        })
-    }
+  skip(_skip: number) {
+    return this.subQuery({
+      ...this.$query,
+      _skip,
+    });
+  }
 
-    limit(_limit: number) {
-        return this.subQuery({
-            ...this.$query,
-            _limit,
-        })
-    }
+  $hydrate(data: OpaqueRow[]) {
+    return data.map((attributes) => this.model.$fromRow(attributes));
+  }
 
-    skip(_skip: number) {
-        return this.subQuery({
-            ...this.$query,
-            _skip,
-        })
-    }
+  async get() {
+    return this.$hydrate(await this.model.$adapter.read(this.$query));
+  }
+  async update(data: Partial<ModelAttributes<InstanceType<Model>>>) {
+    return this.$hydrate(await this.model.$adapter.update(this.$query, data));
+  }
+  async delete() {
+    return await this.model.$adapter.delete(this.$query);
+  }
 
-    $hydrate(data: OpaqueRow[]) {
-        return data.map(attributes => this.model.$fromRow(attributes))
-    }
+  async first() {
+    return (await this.limit(1).get())[0];
+  }
 
-    async get() {
-        return this.$hydrate(await this.model.$adapter.read(this.$query))
-    }
-    async update(data: Partial<ModelAttributes<InstanceType<Model>>>) {
-        return this.$hydrate(await this.model.$adapter.update(this.$query, data))
-    }
-    async delete() {
-        return await this.model.$adapter.delete(this.$query)
-    }
+  getSync(
+    this: QueryBuilder<Model & { adapter: () => SyncReadAdapterContract }>
+  ) {
+    return this.$hydrate(this.model.$getAdapter().readSync(this.$query));
+  }
+  updateSync(
+    this: QueryBuilder<Model & { adapter: () => SyncWriteAdapterContract }>,
+    data: OpaqueRow
+  ) {
+    return this.$hydrate(
+      this.model.$getAdapter().updateSync(this.$query, data)
+    );
+  }
+  deleteSync(
+    this: QueryBuilder<Model & { adapter: () => SyncWriteAdapterContract }>
+  ) {
+    return this.model.$getAdapter().deleteSync(this.$query);
+  }
 
-    async first() {
-        return (await this.limit(1).get())[0]
-    }
-
-    getSync(this: QueryBuilder<Model & { adapter: () => SyncReadAdapterContract }>) {
-        return this.$hydrate(this.model.$getAdapter().readSync(this.$query))
-    }
-    updateSync(this: QueryBuilder<Model & { adapter: () => SyncWriteAdapterContract }>, data: OpaqueRow) {
-        return this.$hydrate(this.model.$getAdapter().updateSync(this.$query, data))
-    }
-    deleteSync(this: QueryBuilder<Model & { adapter: () => SyncWriteAdapterContract }>) {
-        return this.model.$getAdapter().deleteSync(this.$query)
-    }
-
-    firstSync(this: QueryBuilder<Model & { adapter: () => SyncReadAdapterContract }>): InstanceType<Model> {
-        return this.limit(1).getSync()[0]
-    }
+  firstSync(
+    this: QueryBuilder<Model & { adapter: () => SyncReadAdapterContract }>
+  ): InstanceType<Model> {
+    return this.limit(1).getSync()[0];
+  }
 }
